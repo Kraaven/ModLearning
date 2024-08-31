f976955fa0f2f9fd91326e4a2e6a83bcc059c061
Kraaven <106032882+Kraaven@users.noreply.github.com>
2024-08-31 10:46:09 +0530

    Basic Setup

---
+++ src/main/java/com/kraaven/modlearning/ModdingLearningClient.java
@@ -0,0 +1,10 @@
+package com.kraaven.modlearning;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModdingLearningClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
